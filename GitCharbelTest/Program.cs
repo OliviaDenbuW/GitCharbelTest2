c168a0bdfc5b41a66351eb92d3025e7c824e8c49
Olivia Considerit <olivia.denbu@gmail.com>
2018-11-12 15:57:22 +0100

    Add project files.

---
+++ GitCharbelTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitCharbelTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
